9cfe87d2d3bc5a0bf8bf432aa2a57302e9d4e813
Robert French <rfrench@seti.org>
2019-04-30 10:12:54 -0700

    Fix server disk test

--- perf_test/server_disk_perf.sh
+++ perf_test/server_disk_perf.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "*** DISK READ ***"
-dd if=/dev/zero of=/tmp/ddtest.out bs=1G count=2 oflag=dsync
+echo 1 > /proc/sys/vm/drop_caches
+sleep 1
 echo "*** DISK WRITE ***"
-dd if=/tmp/ddtest.out of=/dev/null bs=1G count=2 oflag=dsync
+dd if=/dev/zero of=/tmp/ddtest.out bs=1G count=5 oflag=dsync
+echo 1 > /proc/sys/vm/drop_caches
+sleep 1
+echo "*** DISK READ ***"
+dd if=/tmp/ddtest.out of=/dev/null bs=1G count=5 oflag=dsync
 rm /tmp/ddtest.out
